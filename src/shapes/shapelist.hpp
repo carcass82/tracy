cb0fc59c51ffee7f3076828f54dafc3e60680ecf
Carlo Casta <carlo.casta@gmail.com>
2019-09-22 04:45:01 +0200

    minor alloc optimization

--- src/shapes/shapelist.hpp
+++ src/shapes/shapelist.hpp
@@ -17,12 +17,10 @@ public:
     ShapeList(IShape** objects, int num, IMaterial* force_material = nullptr)
         : debug_material(force_material)
     {
-        boxes = new Box*[num];
-        spheres = new Sphere*[num];
-        triangles = new Triangle*[num];
-        lists = new ShapeList*[num];
-
-        materials = new IMaterial*[num];
+        boxes = new Box[num];
+        spheres = new Sphere[num];
+        triangles = new Triangle[num];
+        lists = new ShapeList[num];
 
         for (int i = 0; i < num; ++i)
         {
@@ -31,21 +29,19 @@ public:
             bbox.expand(outmin);
             bbox.expand(outmax);
 
-            materials[material_count++] = objects[i]->get_material();
-
             switch (objects[i]->get_id())
             {
             case make_id('B','O','X'):
-                boxes[box_count++] = static_cast<Box*>(objects[i]);
+                memcpy(&boxes[box_count++], objects[i], sizeof(Box));
                 break;
             case make_id('S','P','H'):
-                spheres[sphere_count++] = static_cast<Sphere*>(objects[i]);
+				memcpy(&spheres[sphere_count++], objects[i], sizeof(Sphere));
                 break;
             case make_id('T','R','I'):
-                triangles[triangle_count++] = static_cast<Triangle*>(objects[i]);
+				memcpy(&triangles[triangle_count++], objects[i], sizeof(Triangle));
                 break;
             case make_id('L','I','S','T'):
-                lists[list_count++] = static_cast<ShapeList*>(objects[i]);
+				memcpy(&lists[list_count++], objects[i], sizeof(ShapeList));
                 break;
             default:
                 break;
@@ -55,12 +51,10 @@ public:
 
     ~ShapeList()
     {
-        for (int i = 0; i < box_count; ++i)      { delete boxes[i];     } delete[] boxes;
-        for (int i = 0; i < sphere_count; ++i)   { delete spheres[i];   } delete[] spheres;
-        for (int i = 0; i < triangle_count; ++i) { delete triangles[i]; } delete[] triangles;
-        for (int i = 0; i < list_count; ++i)     { delete lists[i];     } delete[] lists;
-
-        for (int i = 0; i < material_count; ++i) { delete materials[i]; } delete[] materials;
+        delete[] boxes;
+        delete[] spheres;
+        delete[] triangles;
+        delete[] lists;
     }
 
     virtual bool hit(const Ray& r, float t_min, float t_max, HitData& rec) const override final
@@ -77,7 +71,7 @@ public:
             bool hit_list = false;
             for (int i = 0; i < list_count; ++i)
             {
-                if (lists[i]->hit(r, t_min, temp_rec.t, temp_rec))
+                if (lists[i].hit(r, t_min, temp_rec.t, temp_rec))
                 {
                     hit_list = true;
                 }
@@ -92,9 +86,9 @@ public:
             bool hit_sphere = false;
             for (int i = 0; i < sphere_count; ++i)
             {
-                vec3 oc{ r.get_origin() - spheres[i]->center };
+                vec3 oc{ r.get_origin() - spheres[i].center };
                 float b = dot(oc, r.get_direction());
-                float c = dot(oc, oc) - spheres[i]->radius2;
+                float c = dot(oc, oc) - spheres[i].radius2;
 
                 if (b <= .0f || c <= .0f)
                 {
@@ -123,7 +117,7 @@ public:
             }
             if (hit_sphere)
             {
-                spheres[closest_index_so_far]->get_hit_data(r, temp_rec);
+                spheres[closest_index_so_far].get_hit_data(r, temp_rec);
             }
             //
             // check spheres
@@ -140,8 +134,8 @@ public:
                 float tmax = temp_rec.t;
                 bool hit = false;
 
-                const vec3 minbound = (boxes[i]->pmin - r.get_origin()) * inv_ray;
-                const vec3 maxbound = (boxes[i]->pmax - r.get_origin()) * inv_ray;
+                const vec3 minbound = (boxes[i].pmin - r.get_origin()) * inv_ray;
+                const vec3 maxbound = (boxes[i].pmax - r.get_origin()) * inv_ray;
                 for (int dim = 0; dim < 3; ++dim)
                 {
                     float t1 = minbound[dim];
@@ -167,7 +161,7 @@ public:
             }
             if (hit_box)
             {
-                boxes[closest_index_so_far]->get_hit_data(r, temp_rec);
+                boxes[closest_index_so_far].get_hit_data(r, temp_rec);
             }
             //
             // check boxes
@@ -179,8 +173,8 @@ public:
             bool hit_triangle = false;
             for (int i = 0; i < triangle_count; ++i)
             {
-                vec3 pvec = cross(r.get_direction(), triangles[i]->v0v2);
-                float det = dot(triangles[i]->v0v1, pvec);
+                vec3 pvec = cross(r.get_direction(), triangles[i].v0v2);
+                float det = dot(triangles[i].v0v1, pvec);
 
                 // if the determinant is negative the triangle is backfacing
                 // if the determinant is close to 0, the ray misses the triangle
@@ -191,24 +185,24 @@ public:
 
                 float invDet = 1.f / det;
 
-                vec3 tvec = r.get_origin() - triangles[i]->vertices[0];
+                vec3 tvec = r.get_origin() - triangles[i].vertices[0];
                 float u = dot(tvec, pvec) * invDet;
                 if (u < .0f || u > 1.f)
                 {
                     continue;
                 }
 
-                vec3 qvec = cross(tvec, triangles[i]->v0v1);
+                vec3 qvec = cross(tvec, triangles[i].v0v1);
                 float v = dot(r.get_direction(), qvec) * invDet;
                 if (v < .0f || u + v > 1.f)
                 {
                     continue;
                 }
 
-                float t = dot(triangles[i]->v0v2, qvec) * invDet;
+                float t = dot(triangles[i].v0v2, qvec) * invDet;
                 if (t < temp_rec.t && t > t_min)
                 {
-                    temp_rec.t = dot(triangles[i]->v0v2, qvec) * invDet;
+                    temp_rec.t = dot(triangles[i].v0v2, qvec) * invDet;
                     temp_rec.uv = vec2{ u, v };
                     closest_index_so_far = i;
                     hit_triangle = true;
@@ -216,7 +210,7 @@ public:
             }
             if (hit_triangle)
             {
-                triangles[closest_index_so_far]->get_hit_data(r, temp_rec);
+                triangles[closest_index_so_far].get_hit_data(r, temp_rec);
             }
             //
             // check triangles
@@ -292,16 +286,15 @@ private:
         return true;
     }
 
-    Box** boxes = nullptr;
-    Sphere** spheres = nullptr;
-    Triangle** triangles = nullptr;
-    ShapeList** lists = nullptr;
+    Box* boxes = nullptr;
+    Sphere* spheres = nullptr;
+    Triangle* triangles = nullptr;
+    ShapeList* lists = nullptr;
     int box_count = 0;
     int sphere_count = 0;
     int triangle_count = 0;
     int list_count = 0;
 
-    IMaterial** materials = nullptr;
     IMaterial* debug_material = nullptr;
     int material_count = 0;
 
